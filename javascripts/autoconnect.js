aed2865eecdb88ed6ac3631b185b443f3844f509
nobe4 <haffreinguev@gmail.com>
2015-01-28 18:04:16 +0100

    added working script and markup

--- javascripts/autoconnect.js
+++ javascripts/autoconnect.js
@@ -1,1 +1,15 @@
-console.log('loaded');
+$(function(){
+	var baseScript = 'javascript:$("#logonForm_standard_auth_form input").each(function(e,a){a.value = (e==0)?"&login":"&password";});$("#logonForm_connect_button").click();';
+
+	function updateLink(){
+		var login = $("input#login-input").val();
+		var password = $("input#password-input").val();
+
+		var script = baseScript.replace("&login",login).replace("&password",password);
+		console.log(script);
+		$("#bookmarklet-link").attr("href",script);
+	}
+	$("input").on('keyup',function(e){
+		updateLink();
+	});
+});
